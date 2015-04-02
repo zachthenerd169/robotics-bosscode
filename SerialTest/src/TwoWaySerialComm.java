623fa6952c971187ef4a3d4fe5019ce3d91c8956
stephaniek95 <stephanieksmith95@gmail.com>
2015-04-02 18:06:10 -0500

    RXTX java code
    code taken from the RXTX wiki

---
+++ SerialTest/src/TwoWaySerialComm.java
@@ -0,0 +1,126 @@
+
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class TwoWaySerialComm
+{
+    public TwoWaySerialComm()
+    {
+        super();
+    }
+    
+    void connect ( String portName ) throws Exception
+    {
+        CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+        if ( portIdentifier.isCurrentlyOwned() )
+        {
+            System.out.println("Error: Port is currently in use");
+        }
+        else
+        {
+            CommPort commPort = portIdentifier.open(this.getClass().getName(),2000);
+            
+            if ( commPort instanceof SerialPort )
+            {
+                SerialPort serialPort = (SerialPort) commPort;
+                serialPort.setSerialPortParams(9600,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
+                
+                InputStream in = serialPort.getInputStream();
+                OutputStream out = serialPort.getOutputStream();
+                
+                //does this have to be multithreaded??
+                (new Thread(new SerialReader(in))).start();
+                (new Thread(new SerialWriter(out))).start();
+
+            }
+            else
+            {
+                System.out.println("Error: Only serial ports are handled by this example.");
+            }
+        }     
+    }
+    
+    /** */
+    public static class SerialReader implements Runnable 
+    {
+        InputStream in;
+        
+        public SerialReader ( InputStream in ) 
+        {
+            this.in = in;
+         
+   
+			
+			
+        }
+        
+        public void run ()
+        {
+        	System.out.println("In read");
+            byte[] buffer = new byte[1024];
+            int len = -1;
+            try
+            {
+                while ( ( len = this.in.read(buffer)) > -1 )
+                {
+                    System.out.print(new String(buffer,0,len));
+                }
+            }
+            catch ( IOException e )
+            {
+                e.printStackTrace();
+            }            
+        }
+    }
+
+    /** */
+    public static class SerialWriter implements Runnable 
+    {
+        OutputStream out;
+        
+        public SerialWriter ( OutputStream out )
+        {
+            this.out = out;
+        }
+        
+        public void run ()
+        {
+        	System.out.println("In write");
+            try
+            {                
+                int c = 0;
+                while ( ( c<10 ))
+                {
+                	
+                	
+                    this.out.write(c);
+                    this.out.flush();
+                    c++;
+                }                
+            }
+            catch ( IOException e )
+            {
+                e.printStackTrace();
+            }            
+        }
+    }
+    
+    public static void main ( String[] args )
+    {
+        try
+        {
+            (new TwoWaySerialComm()).connect("/dev/tty.usbmodem1411");
+        }
+        catch ( Exception e )
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
