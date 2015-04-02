9adb4bfb6a6462627f2c96c72ee0084fc4b48e5d
lukazd <luka-zdilar@uiowa.edu>
2015-04-02 18:47:24 -0500

    The correct serial file

--- SerialTest.ino
+++ SerialTest.ino
@@ -4,26 +4,40 @@
  * This is the Hello World! for Arduino. 
  * It shows how to send data to the computer
  */
-int led = 11;
+ 
+ #include <Servo.h>
+int led = 13;
+Servo myservo;
 
 void setup()                    // run once, when the sketch starts
 {
   pinMode(led, OUTPUT);
-  digitalWrite(led, HIGH);
-  //Serial.begin(9600);           // set up Serial library at 9600 bps
-  
-  //Serial.println("Hello world!");  // prints hello with ending line break 
+  digitalWrite(led, LOW);
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  myservo.attach(9);
+  Serial.println("Hello world!");  // prints hello with ending line break 
 }
 
 void loop()                       // run over and over again
 {
-  //if(Serial.available() > 0) 
-  //{
-    //turnOn();
-  //}
+  if(Serial.available() > 0) 
+  {
+    float num = Serial.parseFloat();
+    if(num < 180 || num > 0)
+    {
+      myservo.write(num);
+    }
+    if(num == 200)
+    {
+      turnOn();
+    }
+  }
 }
 
 void turnOn()
 {
+  delay(1000);
   digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
 }
