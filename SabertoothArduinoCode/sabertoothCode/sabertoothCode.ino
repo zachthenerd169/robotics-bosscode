fa052893e4af51abf272affcc5d33ef8ac88b547
lukazd <luka-zdilar@uiowa.edu>
2015-04-27 15:49:39 -0500

    Updated motor controller
    Most of this works

--- SabertoothArduinoCode/sabertoothCode/sabertoothCode.ino
+++ SabertoothArduinoCode/sabertoothCode/sabertoothCode.ino
@@ -1,17 +1,39 @@
-
 // Software Serial Sample for Packet Serial
 // Copyright (c) 2012 Dimension Engineering LLC
 // See license.txt for license details.
 #include <SoftwareSerial.h>
 #include <Sabertooth.h>
-SoftwareSerial SWSerial(NOT_A_PIN, 14); // RX on no pin (unused), TX on pin 11 (to S1).
-Sabertooth ST(128, SWSerial); // Address 128, and use SWSerial as the serial port.
-int led = 13;
+#include "DualVNH5019MotorShield.h"
+
+SoftwareSerial SWSerial1(NOT_A_PIN, 14); // RX on no pin (unused), TX on pin 14 (to S1).
+SoftwareSerial SWSerial2(NOT_A_PIN, 16);
+Sabertooth ST1(128, SWSerial1); // Address 128, and use SWSerial as the serial port.
+Sabertooth ST2(128, SWSerial2);
+
+DualVNH5019MotorShield md; // The motor driver for digger actuators
+
+int powerLevels[4] = {20, 50, 90, 127};
+
+void stopIfFault()
+{
+   if(md.getM1Fault())
+   {
+     Serial.println("Fault with digger motors"); 
+   }
+}
+
 void setup()
 {
   Serial.begin(9600);
-  SWSerial.begin(9600);
-  ST.autobaud();
+  SWSerial1.begin(9600);
+  SWSerial2.begin(9600);
+  ST1.autobaud();
+  ST2.autobaud();
+  ST1.setRamping(55);
+  ST2.setRamping(55);
+  
+  md.init(); // initialize motor driver
+
 }
 
 void loop()
@@ -19,54 +41,130 @@ void loop()
   if(Serial.available() > 0) 
   {
     int num = Serial.read();
-    Serial.println(num, DEC);
+    int powerLevel = num%4;
+    int mode = num/4;
     
-    if(num == 100)
+    // Stop
+    if(mode <= 0)
     {
-      digitalWrite(led, HIGH);
-      turn(1,100);
+       stopRobot(); 
     }
-    else if(num == 200)
+    
+    // Straight
+    else if(mode == 1)  // This is actually turning
     {
-      digitalWrite(led, LOW);
-       turn(-1,100); 
+       straight(powerLevels[powerLevel]); 
     }
-    else if(num == 300)
+    
+    // Backward
+    else if(mode == 2)  // This is actually turning
     {
-       stop(); 
+      backward(powerLevels[powerLevel]);  
     }
-    else if(num == 400)
+    
+    // Turn Left
+    else if(mode == 3)  // This is actually straight or back
     {
-       straight(100); 
+      turnRobot(1,powerLevels[powerLevel]);
     }
-    else if(num == 500)
+    
+    // Turn Right
+    else if(mode == 4) // This is actually straight or back
     {
-      backwards(100);  
+       turnRobot(-1,powerLevels[powerLevel]); 
     }
+    
+    
+    // Digger Drop
+    else if(mode == 5)
+    {
+      diggerDrop();
+    }  
+    
+     // Digger Up
+    else if(mode == 6)
+    {
+      diggerUp();
+    }  
+    
+    
+     // Bucket Dump
+    else if(mode == 7)
+    {
+      bucketDump();
+    }  
+    
+    // Bucket Down
+    else if(mode == 8)
+    {
+      bucketDown();
+    }  
+    
+    /*
+     // Strafe right
+    else if(mode == 9)
+    {
+      strafeRight();
+    }  
+    
+     // Strafe left
+    else if(mode == 10)
+    {
+      strafeLeft();
+    }  
+    */
   }
 }
 
-void turn(int direction, int powerLevel)
-{
-    ST.motor(1, -direction * powerLevel);
-    ST.motor(2, -direction * powerLevel);
-}
 
-void stop()
+
+void stopRobot()
 {
-   ST.motor(1, 0);
-   ST.motor(2, 0);
+   ST1.motor(1, 0);
+   ST1.motor(2, 0);
 }
 
 void straight(int powerLevel)
 {
-    ST.motor(1, powerLevel);
-    ST.motor(2, -powerLevel);
+    ST1.motor(1, powerLevel);
+    ST1.motor(2, -powerLevel);
 }
 
-void backwards(int powerLevel)
+void backward(int powerLevel)
 {
-    ST.motor(1, -powerLevel);
-    ST.motor(2, powerLevel);
+    ST1.motor(1, -powerLevel);
+    ST1.motor(2, powerLevel);
 }
+
+void turnRobot(int direction, int powerLevel)
+{
+    ST1.motor(1, direction * powerLevel);
+    ST1.motor(2, direction * powerLevel);
+}
+
+void diggerUp()
+{
+   ST2.motor(1, -127);  // Linear actuator out 
+   ST2.motor(2, 0);  // Motor on
+}
+
+void diggerDrop()
+{
+   ST2.motor(1, 127);   // Linear actuator in
+   ST2.motor(2, 127);    // Motor off
+}
+
+void bucketDump()
+{
+   md.setSpeeds(-400, -400);  // Linear actuators up
+   stopIfFault();
+}
+
+void bucketDown()
+{
+   md.setSpeeds(400, 400);  // Linear actuators down
+   stopIfFault();
+}
+
+
 
