b54b1a57393aaf753237ae96e8ca0714575eba42
Stephanie Smith <stephaniesmith@Stephanies-MacBook-Pro.local>
2016-01-13 12:47:56 -0600

    creatingclient

---
+++ TCP_C/Client.cpp
@@ -0,0 +1,60 @@
+//  Client.cpp
+//  Created by Stephanie Smith on 1/13/16.
+//
+//  Description: client (the laptop running this code) will communicate
+//  with the NUC (server) on the robot
+//
+//  To Compile: g++ -o Client.exe Client.cpp PracticalSocket.cpp
+//  To Run: ./Client.exe 'Server's IP Address' 'Server's Port Number'
+
+#include "PracticalSocket.h"  // For Socket and SocketException
+#include "Client.hpp"
+#include <iostream>           // For cerr and cout
+#include <cstdlib>            // For atoi()
+
+const int RCVBUFSIZE = 32;    // Size of receive buffer
+
+int main (int argc, char *argv[])
+{
+    
+    if(argc!=3){cerr<<"\n\nError: Wrong Number of Arguments\nTo Run: "<< argv[0]<<" <Server's IP Address> <Server's Port>\n\n"<<endl;
+        exit(1);} //imediately terminates if command line arguments were not specified
+    
+    bool done = true; //flag for main-menu do-while
+    int userSelection=0; //user input for menu's
+    char userConfirm;
+    string servAddress=argv[1];
+    unsigned short servPort = atoi(argv[2]);
+    string message; //message client will send the server
+    //confirm input (don't know if I should keep this)
+    cout<<"\nServer IP is: "<<servAddress<<"\nServer Port is: "<<servPort<<"\nAre these correct?(Y/N)"<<endl;
+    cin>>userConfirm;
+    while(userConfirm!='Y' && userConfirm!='y' && userConfirm!='N' && userConfirm!='n')
+    {
+        cout<<"Please input 'Y' or 'N'"<<endl;
+    }
+    if(userConfirm=='N' || userConfirm == 'n')
+    {
+        cout<<"run again with different arguments"<<endl;
+        exit(1);
+    }
+    
+    do //main menu
+    {
+        cout<<"\nUofI Robotic's Control Menu: "<<endl;
+        cout<<"1) CONTROL ROBOT\n2) Change Server's IP Address\n3) Change Server's Port Number\n4) Send Test Message To Server Without Arduino\n5) Send Test Message To Server With Arduino\n6) Quit Program\nEnter a number that is 1-6"<<endl;
+        cin>>userSelection;
+        cout<<"user selected "<<userSelection<<endl;
+        switch (userSelection)
+        {
+            case 6:
+                done=false;
+                cout<<"Quiting Program!"<<endl;
+                break;
+                
+            default:
+                cout<<"Enter a number that is 1-6"<<endl;
+        }
+    }
+    while(done);
+}
---
+++ TCP_C/Client.hpp
@@ -0,0 +1,14 @@
+//
+//  Client.hpp
+//  
+//
+//  Created by Stephanie Smith on 1/13/16.
+//
+//
+
+#ifndef Client_hpp
+#define Client_hpp
+
+#include <stdio.h>
+
+#endif /* Client_hpp */
