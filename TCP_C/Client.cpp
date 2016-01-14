7ffc747cd071e5f04351345fed280d21aff2955f
Stephanie Smith <stephanieksmith95@gmail.com>
2016-01-13 20:50:36 -0600

    handling user input

--- TCP_C/Client.cpp
+++ TCP_C/Client.cpp
@@ -6,13 +6,12 @@
 //
 //  To Compile: g++ -o Client.exe Client.cpp PracticalSocket.cpp
 //  To Run: ./Client.exe 'Server's IP Address' 'Server's Port Number'
-///fdsfafddsfdsafdsa
 
 #include "PracticalSocket.h"  // For Socket and SocketException
 #include "Client.hpp"
 #include <iostream>           // For cerr and cout
 #include <cstdlib>            // For atoi()
-
+#include <string>           // For user inputs
 const int RCVBUFSIZE = 32;    // Size of receive buffer
 
 int main (int argc, char *argv[])
@@ -27,12 +26,17 @@ int main (int argc, char *argv[])
     string servAddress=argv[1];
     unsigned short servPort = atoi(argv[2]);
     string message; //message client will send the server
+   
     //confirm input (don't know if I should keep this)
     cout<<"\nServer IP is: "<<servAddress<<"\nServer Port is: "<<servPort<<"\nAre these correct?(Y/N)"<<endl;
     cin>>userConfirm;
-    while(userConfirm!='Y' && userConfirm!='y' && userConfirm!='N' && userConfirm!='n')
+    bool valid = getchar() == '\n' ? true : false;
+    while((!valid) || (userConfirm!='Y' && userConfirm!='y' && userConfirm!='N' && userConfirm!='n'))
     {
         cout<<"Please input 'Y' or 'N'"<<endl;
+        if(!valid){while ( getchar() != '\n' );} //flusing input buffer
+        cin>>userConfirm;
+        valid = getchar() == '\n' ? true : false;
     }
     if(userConfirm=='N' || userConfirm == 'n')
     {
@@ -44,8 +48,17 @@ int main (int argc, char *argv[])
     {
         cout<<"\nUofI Robotic's Control Menu: "<<endl;
         cout<<"1) CONTROL ROBOT\n2) Change Server's IP Address\n3) Change Server's Port Number\n4) Send Test Message To Server Without Arduino\n5) Send Test Message To Server With Arduino\n6) Quit Program\nEnter a number that is 1-6"<<endl;
+     
         cin>>userSelection;
+        while(cin.fail()) //error handling user input
+        {
+            std::cin.clear();
+            std::cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            std::cout << "Error: Input is not an integer\nEnter a number that is 1-6"<<endl;
+            std::cin >> userSelection;
+        }
         cout<<"user selected "<<userSelection<<endl;
+        
         switch (userSelection)
         {
             case 6:
