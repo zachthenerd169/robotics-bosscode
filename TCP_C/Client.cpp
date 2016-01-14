6c06c9181b0858207a54f769838dd67f3dd2e1c1
Stephanie Smith <stephanieksmith95@gmail.com>
2016-01-13 21:21:39 -0600

    moved some stuff in functions

--- TCP_C/Client.cpp
+++ TCP_C/Client.cpp
@@ -14,6 +14,9 @@
 #include <string>           // For user inputs
 const int RCVBUFSIZE = 32;    // Size of receive buffer
 
+bool confirm(char userConfirm, bool valid); //prototypes
+int inputInt(void);
+
 int main (int argc, char *argv[])
 {
     
@@ -31,25 +34,14 @@ int main (int argc, char *argv[])
     cout<<"\nServer IP is: "<<servAddress<<"\nServer Port is: "<<servPort<<"\nAre these correct?(Y/N)"<<endl;
     cin>>userConfirm;
     bool valid = getchar() == '\n' ? true : false;
-    while((!valid) || (userConfirm!='Y' && userConfirm!='y' && userConfirm!='N' && userConfirm!='n'))
-    {
-        cout<<"Please input 'Y' or 'N'"<<endl;
-        if(!valid){while ( getchar() != '\n' );} //flusing input buffer
-        cin>>userConfirm;
-        valid = getchar() == '\n' ? true : false;
-    }
-    if(userConfirm=='N' || userConfirm == 'n')
-    {
-        cout<<"run again with different arguments"<<endl;
-        exit(1);
-    }
-    
+    if(!confirm(userConfirm, valid)){exit(1);}
     do //main menu
     {
         cout<<"\nUofI Robotic's Control Menu: "<<endl;
         cout<<"1) CONTROL ROBOT\n2) Change Server's IP Address\n3) Change Server's Port Number\n4) Send Test Message To Server Without Arduino\n5) Send Test Message To Server With Arduino\n6) Quit Program\nEnter a number that is 1-6"<<endl;
      
-        cin>>userSelection;
+        userSelection = inputInt();
+        /*cin>>userSelection;
         while(cin.fail()) //error handling user input
         {
             std::cin.clear();
@@ -57,10 +49,43 @@ int main (int argc, char *argv[])
             std::cout << "Error: Input is not an integer\nEnter a number that is 1-6"<<endl;
             std::cin >> userSelection;
         }
-        cout<<"user selected "<<userSelection<<endl;
+        cout<<"user selected "<<userSelection<<endl;*/
         
         switch (userSelection)
         {
+            case 1:
+                cout<<"haven't implemented"<<endl;
+                break;
+            case 2:
+                cout<<"current address is: "<<servAddress<<"\nEnter new address: ";
+                do //keep asking for the address until the user confirms what they have entered
+                {
+                    cin>>servAddress;
+                    cout<<"\nnew address is: "<<servAddress<<endl;
+                    cout<<"Is this correct?(Y/N)"<<endl;
+                    cin>>userConfirm;
+                    valid = getchar() == '\n' ? true : false;
+                }
+                while(!confirm(userConfirm, valid));
+                break;
+            case 3:
+                cout<<"current port is: "<<servPort<<"\nEnter new Port ";
+                do
+                {
+                    servPort=inputInt();
+                    cout<<"\nnew port is: "<<servPort<<endl;
+                    cout<<"Is this correct?(Y/N)"<<endl;
+                    cin>>userConfirm;
+                    valid = getchar() == '\n' ? true : false;
+                }
+                while(!confirm(userConfirm, valid));
+                break;
+            case 4:
+                cout<<"haven't implemented"<<endl;
+                break;
+            case 5:
+                cout<<"haven't implemented"<<endl;
+                break;
             case 6:
                 done=false;
                 cout<<"Quiting Program!"<<endl;
@@ -71,4 +96,44 @@ int main (int argc, char *argv[])
         }
     }
     while(done);
+}
+/**
+ * Input: the user input (should be a 'Y' or an 'N')
+ *        a boolean that is true when the user enter a single character and false otherwise
+ *
+ *  Output: false if the user says no ('N') true otherwisse
+ **/
+bool confirm(char userConfirm, bool valid)
+{
+    while((!valid) || (userConfirm!='Y' && userConfirm!='y' && userConfirm!='N' && userConfirm!='n'))
+    {
+        cout<<"Please input 'Y' or 'N'"<<endl;
+        if(!valid){while ( getchar() != '\n' );} //flusing input buffer
+        cin>>userConfirm;
+        valid = getchar() == '\n' ? true : false;
+    }
+    if(userConfirm=='N' || userConfirm == 'n')
+    {
+        cout<<"run again with different arguments"<<endl;
+        return false;
+    }
+    else{return true;} //user confirmed with 'Y'
+
+}
+/**
+ * function outputs the number the user has inputted
+ **/
+int inputInt(void)
+{
+    int input;
+    cin>>input;
+    while(cin.fail()) //error handling user input
+    {
+        std::cin.clear();
+        std::cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        std::cout << "Error: Input is not an integer\nTry again"<<endl;
+        std::cin >> input;
+    }
+    //cout<<"user selected "<<input<<endl;
+    return input;
 }
