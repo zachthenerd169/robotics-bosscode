92c7ae1ad29c50b9734d0f229d87b1134ef47b14
Stephanie Smith <stephanieksmith95@gmail.com>
2016-04-10 14:58:28 -0500

    added socket library

--- ControlSystem/Network.h
+++ ControlSystem/Network.h
@@ -8,8 +8,13 @@
 #ifndef NETWORK_H_
 #define NETWORK_H_
 
-
-
-
+class Network
+{
+	public:
+	private:
+		//TCPSocket
+		CommandBuffer* m_command_buffer;
+		DataBuffer* m_data_buffer;
+};
 
 #endif /* NETWORK_H_ */
