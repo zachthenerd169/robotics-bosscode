b2459cfc1d3d5a3a404f6ebb924e68212ed4b48a
Stephanie Smith <stephanieksmith95@gmail.com>
2016-04-10 20:13:54 -0500

    xbox controller

--- ControlSystem/XboxController.h
+++ ControlSystem/XboxController.h
@@ -1,8 +1,9 @@
 /*
  * XboxController.h
- *
- *  Created on: Apr 7, 2016
- *      Author: stephaniesmith
+ * Created on: Apr 7, 2016
+ * Author: stephaniesmith
+ * Class Desciption:
+ * Control Key:
  */
 
 #ifndef XBOXCONTROLLER_H_
@@ -11,8 +12,31 @@
 class XboxController : UserController
 {
 	public:
-		bool processInput();
+		/**
+		 * Description: constructor copies TCPSocket, xbox controller is initialized so the nothing is pressed
+		 * Inputs: a TCPSocket object
+		 */
+		XboxController(TCPSocket* socket):UserController(socket),m_xbox_controller({0,0,0,0,0,0,0,0,0,0,0,0}){};
+		/**
+		 * Description: creates a TCPSocket objects, xbox controller is initialized so the nothing is pressed
+		 * Inputs: the IP Address of the NUC (the computer on the robot)
+		 * 		   the port number the NUC is listening on
+		 */
+		XboxController(std::string address, unsigned short port):
+				UserController(address, port),m_xbox_controller({0,0,0,0,0,0,0,0,0,0,0,0}){};
+		/**
+		 * Description: <<how will this method work?>>
+		 * Inputs: string that stores the processed input that is to be sent to the robot
+		 * Outputs: returns true if command was successfully processed, returns false
+		 * 			otherwise.
+		 */
+		bool processInput(std::string &processed_input);
 	private:
+		/**
+		 * holds the state of the xbox controller
+		 * if a member value is false it is not pressed
+		 * if a member value is true it is pressed
+		 */
 		struct ControllerState
 		{
 			bool endA;
@@ -26,12 +50,7 @@ class XboxController : UserController
 			bool endLB;
 			bool endRThumb;
 			bool endLThumb;
-		};
+		}m_xbox_controller;
 
 };
-
-
-
-
-
 #endif /* XBOXCONTROLLER_H_ */
