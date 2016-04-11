a1ce8b83397acd21f48d9626b8e791362a337742
Stephanie Smith <stephanieksmith95@gmail.com>
2016-04-10 20:56:11 -0500

    updated network

--- ControlSystem/Network.h
+++ ControlSystem/Network.h
@@ -1,11 +1,14 @@
 /*
  * Network.h
+ * Created on: Apr 7, 2016
+ * Author: stephaniesmith
+ * Class Description:
  *
- *  Created on: Apr 7, 2016
- *      Author: stephaniesmith
+ * -- maybe ManualProcessor should have a network, otherwise how will Network know when
+ *    to send sensor data back to the user?
  */
 
-#include "lib/PracticalSocket.h"
+#include "lib/PracticalSocket.h" //for m_socket
 
 #ifndef NETWORK_H_
 #define NETWORK_H_
@@ -13,22 +16,40 @@
 class Network
 {
 	public:
-
-		//Network(){m_socket=new TCPSocket;} need some sort of constructor
 		/**
-		 * gets the command from the user and sets that command
-		 * in the command buffer
+		 * Description:
+		 * Inputs:
+		 * Outputs:
 		 */
-		void receiveCommand();
+		Network(std::string address, unsigned short port, CommandBuffer* in_buffer, DataBuffer* out_buffer):
+				m_socket(new TCPSocket(address, port)), m_in_buffer(in_buffer), m_out_buffer(out_buffer){}
 		/**
-		 * gets the data from the data buffer and sends it to the user
+		 * Description:
+		 * Inputs:
+		 * Outputs:
+		 */
+		Network(TCPSocket* socket, CommandBuffer* in_buffer, DataBuffer* out_buffer):
+			m_socket(socket), m_in_buffer(in_buffer), m_out_buffer(out_buffer){}
+		/**
+		 * Decription: receives data from the user through the TCPSocket. after the data
+		 * 			   is received, it will add the data to the in_buffer
+		 *  Inputs: none
+		 *  Outputs: none
+		 */
+		void receiveData();
+		/**
+		 * Description: sends data back to the user.
+		 *  			this data could either be sensor data could either be sensor
+		 *  			data or debugging information
+		 *  Inputs: none
+		 *  Outputs: none
 		 */
 		void sendData();
 
 	private:
 		TCPSocket* m_socket;
-		CommandBuffer* m_command_buffer;
-		DataBuffer* m_data_buffer;
+		CommandBuffer* m_in_buffer;
+		DataBuffer* m_out_buffer;
 };
 
 #endif /* NETWORK_H_ */
