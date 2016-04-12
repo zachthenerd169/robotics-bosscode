cc0056567132e8f6bcfde31e3a6e611c3f1278ad
Stephanie Smith <stephanieksmith95@gmail.com>
2016-04-12 18:53:28 -0500

    updated network constructor

--- ControlSystem/Network.h
+++ ControlSystem/Network.h
@@ -4,11 +4,10 @@
  * Author: stephaniesmith
  * Class Description:
  *
- * -- maybe ManualProcessor should have a network, otherwise how will Network know when
- *    to send sensor data back to the user?
  */
 
 #include "lib/PracticalSocket.h" //for m_socket
+#include <memory> //for smart pointer
 
 #ifndef NETWORK_H_
 #define NETWORK_H_
@@ -21,14 +20,14 @@ class Network
 		 * Inputs:
 		 * Outputs:
 		 */
-		Network(std::string address, unsigned short port, CommandBuffer* in_buffer, DataBuffer* out_buffer):
+		Network(std::string address, unsigned short port, const CommandBuffer& in_buffer, DataBuffer& out_buffer):
 				m_socket(new TCPSocket(address, port)), m_in_buffer(in_buffer), m_out_buffer(out_buffer){}
 		/**
 		 * Description:
 		 * Inputs:
 		 * Outputs:
 		 */
-		Network(TCPSocket* socket, CommandBuffer* in_buffer, DataBuffer* out_buffer):
+		Network(TCPSocket& socket, CommandBuffer& in_buffer, DataBuffer& out_buffer):
 			m_socket(socket), m_in_buffer(in_buffer), m_out_buffer(out_buffer){}
 		/**
 		 * Decription: receives data from the user through the TCPSocket. after the data
@@ -40,16 +39,17 @@ class Network
 		/**
 		 * Description: sends data back to the user.
 		 *  			this data could either be sensor data could either be sensor
-		 *  			data or debugging information
+		 *  			data or debugging information. If there is any data in the data buffer
+		 *  			it will automatically send it back to the user.
 		 *  Inputs: none
 		 *  Outputs: none
 		 */
 		void sendData();
 
 	private:
-		TCPSocket* m_socket;
-		CommandBuffer* m_in_buffer;
-		DataBuffer* m_out_buffer;
+		std::shared_ptr<TCPSocket> m_socket;
+		std::shared_ptr<CommandBuffer> m_in_buffer;
+		std::shared_ptr<DataBuffer> m_out_buffer;
 };
 
 #endif /* NETWORK_H_ */
