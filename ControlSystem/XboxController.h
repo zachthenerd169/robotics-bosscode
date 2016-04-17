c2b471734a11d065826d658829485faee306d1cf
lukazd <luka-zdilar@uiowa.edu>
2016-04-17 18:06:14 -0500

    XboxController header additions
    - power level attribute (and setter)
    - max power level attribute
    - input state
    - controller class

--- ControlSystem/XboxController.h
+++ ControlSystem/XboxController.h
@@ -8,6 +8,8 @@
  * Control Key:
  */
 
+#include "CXBOXController.h"
+
 #ifndef XBOXCONTROLLER_H_
 #define XBOXCONTROLLER_H_
 
@@ -18,19 +20,19 @@ class XboxController : UserController
 		 * Description: constructor copies TCPSocket, xbox controller is initialized so the nothing is pressed
 		 * Inputs: a TCPSocket object
 		 */
-		XboxController(TCPSocket& socket):UserController(socket),m_xbox_controller({0,0,0,0,0,0,0,0,0,0,0,0}){};
+		XboxController(TCPSocket& socket):UserController(socket),m_xbox_controller({1,1,1,1,1,1,1,1,1,1,1,1}){};
 		/**
 		 * Description: creates a TCPSocket objects, xbox controller is initialized so the nothing is pressed
 		 * Inputs: the IP Address of the NUC (the computer on the robot)
 		 * 		   the port number the NUC is listening on
 		 */
 		XboxController(std::string address, unsigned short port):
-				UserController(address, port),m_xbox_controller({0,0,0,0,0,0,0,0,0,0,0,0}){};
+				UserController(address, port),m_xbox_controller({1,1,1,1,1,1,1,1,1,1,1,1}){};
 		/**
 		 * Description: default constructor
 		 * Input: none
 		 */
-		XboxController():UserController(),m_xbox_controller({0,0,0,0,0,0,0,0,0,0,0,0}){};
+		XboxController():UserController(),m_xbox_controller({1,1,1,1,1,1,1,1,1,1,1,1}){};
 		/**
 		 * Description: <<how will this method work?>>
 		 * Inputs: string that stores the processed input that is to be sent to the robot
@@ -38,13 +40,23 @@ class XboxController : UserController
 		 * 			otherwise.
 		 */
 		bool processInput(std::string &processed_input);
+		/**
+		 * Description: setters for class attributes
+		 * Input: menu state or user input
+		 */
+		void setPowerLevel(std::string powerLevel){m_power_level=powerLevel;}
+
+		/**
+		* constant that specifies the absolute largest power level
+		*/
+		static const int MAX_POWER_LEVEL = 50;
 	private:
 		/**
-		 * holds the state of the xbox controller
-		 * if a member value is false it is not pressed
-		 * if a member value is true it is pressed
+		 * holds the button press state of the xbox controller
+		 * if a member value is false the button is being held
+		 * if a member value is true the button is released
 		 */
-		struct ControllerState
+		struct ButtonState
 		{
 			bool endA;
 			bool endB;
@@ -58,6 +70,22 @@ class XboxController : UserController
 			bool endRThumb;
 			bool endLThumb;
 		}m_xbox_controller;
+
+		/**
+		* holds the current power level as adjusted
+		* by the controller
+		*/
+		int m_power_level;
+
+		/**
+		* holds the current state of the xbox controller
+		*/
+		XINPUT_STATE m_state;
+
+		/**
+		* points to the controller being used
+		*/
+		CXBOXController* m_player_controller;
 
 };
 #endif /* XBOXCONTROLLER_H_ */
