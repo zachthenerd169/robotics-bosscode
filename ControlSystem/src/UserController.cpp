fe9f9af4716891669540684c2a356ee437183766
zthroo <pgoldenstein@gmail.com>
2016-05-12 18:34:22 -0500

    all the errors :(

--- ControlSystem/src/UserController.cpp
+++ ControlSystem/src/UserController.cpp
@@ -1,21 +1,24 @@
 #include "../UserController.h"
 #include <iostream>
+//#include <stdio.h>
+//#include <string.h>
 
 
 bool UserController::sendData(std::string data)
 {
 	std::cout<<"sending: "<<data<<std::endl;
-    try
-    {
+    // try
+    // {
 
-        m_socket -> send(data.c_str(), strlen(data.c_str())); //send message to server
-        return true;
-    }
-    catch(SocketException &e)
-    {
-        std::cerr << e.what() << std::endl;
-        return false;
-    }
+    //     m_socket -> send(data.c_str(), strlen(data.c_str())); //send message to server
+    //     return true;
+    // }
+    // catch(SocketException &e)
+    // {
+    //     std::cerr << e.what() << std::endl;
+    //     return false;
+    // }
+    return true;
 }
 std::string UserController::receiveData()
 {
