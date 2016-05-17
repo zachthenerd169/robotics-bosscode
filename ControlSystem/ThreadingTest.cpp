2b2ec0e5f15fd50cdae5f427bf1ea1e8ec49a681
thor <thor@thor-desktop>
2016-05-17 08:44:17 -0400

    fixed comma error in createMotorCommand method in ControlSystem.cpp line 36. there was a comma between unsigned char and dir

---
+++ ControlSystem/ThreadingTest.cpp
@@ -0,0 +1,39 @@
+/* Compile with: g++ -Wall -std=c++11 -o ThreaddedNetworkTest.exe ThreadingTest.cpp  src/ThreaddedNetwork.cpp lib/PracticalSocket.cpp -lpthread
+ * To run: ./ThreaddedNetworkTest.exe
+ */
+#include "ThreaddedNetwork.h"
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2) //Test for correct number of arguments
+  {
+    cerr << "Usage: " << argv[0] << " <Server Port>" << endl;
+    exit(1);
+  }
+  unsigned short servPort = atoi(argv[1]); //First arg: local port
+
+  /* Make the Threaded Network */
+  ThreaddedNetwork network(servPort);
+
+  while(true)
+  {
+    /* get any new messages from the network */
+    std::vector<std::string> data = network.getNewMessages(); 
+    if(!data.empty())
+    {
+      std::cout << "Received new data:\n";
+      network.sendMessage(data.at(0));
+      for(int i=0; i<data.size(); i++)
+      {
+        std::cout << data.at(i) << "\n";
+      }
+      std::cout <<"End new data\n";
+    }
+  }
+
+  return 0;
+}
+
+
+
+
